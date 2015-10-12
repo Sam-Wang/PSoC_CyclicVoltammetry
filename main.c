7785f1016d6d00f51e0d3badacf4e92b3471b826
Kyle Lopin <kvl6@case.edu>
2015-10-12 12:56:02 +0700

    Working cyclic voltammetry setup
    Most features needed to do CV works well.  Timing has been check.  still
    need to make API for it

--- main.c
+++ main.c
@@ -41,11 +41,15 @@ struct wv {
 struct wv waveform;  // create a global of the waveform struct
 
 /* make buffers for the USB ENDPOINTS */
-uint8 IN_Data_Buffer[MAX_NUM_BYTES];// = {1,2,3,4,5,0,0,0,0,0};
+uint8 IN_Data_Buffer[MAX_NUM_BYTES];
 uint8 OUT_Data_Buffer[MAX_NUM_BYTES];
+
+union char_int16_union {
+    uint8 usb_char_data[MAX_NUM_BYTES];
+    int16 usb_int_data[MAX_NUM_BYTES / 2];
+};
+
 char LCD_str[32];  // buffer for LCD screen, make it extra big to avoid overflow
-int IN_EP_count = 0;  // for debug
-int OUT_EP_count = 0;  // for debug
 
 int16 ADC_array[ADC_channels][MAX_LUT_SIZE];  // allocate space to put adc measurements
 uint8 Input_Flag = false;  // if there is an input, set this flag to process it
@@ -58,19 +62,16 @@ uint8 msb_lut_current_value;  // value need to load into msb PIDAC
 uint8 lsb_lut_current_value;  // value to load into lsb PIDAC
 
 /* function prototypes */
-void send_message(char* message, uint8 n);
+void hardware_setup(void);
 void Export_Data(int16 array[], int size);
 uint16 Convert2Dec(uint8 array[], uint8 len);
 void MakeTriangleWave(void);
-void ArrayCheck(void);
 void CheckInput(void);
-void debug_LCD(uint8 row, uint var1, uint var2, uint test_num);
-void button_wait(void);
-
+void USB_load_packet(uint8 packetData[], uint16 length);
 
 CY_ISR(dacInterrupt)
 {
-    //ADC_array[0][lut_index] = PWM_isr_ReadCounter();  // for debug
+    //ADC_array[1][lut_index] = Timer_ReadCounter(); // for testing purposes
     IDAC_msb_SetValue(msb_lut_current_value);
     IDAC_lsb_SetValue(lsb_lut_current_value);
     lut_index++;
@@ -78,109 +79,106 @@ CY_ISR(dacInterrupt)
     {
         isr_dac_Disable();
         isr_adc_Disable();
+        ADC_SigDel_Stop();
         lut_index = 0;         
-        send_message("Done", 32);  // calls a function in an isr but only after the isr has been disabled
-        LCD_Position(1,0);
-        LCD_PrintString("Done running");
+        USB_load_packet((uint8*)"Done", 32); // calls a function in an isr but only after the isr has been disabled
     }
     msb_lut_current_value = msb_waveform_lut[lut_index];
     lsb_lut_current_value = lsb_waveform_lut[lut_index];
-
 }
 CY_ISR(adcInterrupt)
 {
-    //ADC_array[0][lut_index] = PWM_isr_ReadCounter();
-    ADC_StartConvert();
-    ADC_IsEndConversion(ADC_WAIT_FOR_RESULT);
-    ADC_array[0][lut_index] = ADC_GetResult16();
-    //ADC_array[0][lut_index] = read_adc
+    //ADC_array[2][lut_index] = Timer_ReadCounter();// for testing purposes
+    ADC_array[0][lut_index] = ADC_SigDel_GetResult16(); 
+     
+    //  ADC_array[0][lut_index] = (msb_lut_current_value << 3) + lsb_lut_current_value;
 }
 
 int main()
 {
-    /* Place your initialization/startup code here */
-    /* Initialize all the interrupts */
+    /* Initialize all the hardware and interrupts */
+    hardware_setup();
     isr_dac_StartEx(dacInterrupt);
     isr_dac_Disable();  // disable interrupt until a voltage signal needs to be given
     isr_adc_StartEx(adcInterrupt);
-    isr_adc_Disable();
-    
-    /* Initialize all the hardware components */
-    ADC_Start();
-    TIA_Start();
-    VDAC_TIA_Start();
-    Timer_Start();
-    IDAC_msb_Start();
-    IDAC_lsb_Start();
-    PWM_isr_Start();
-    Opamp_Aux_Start();
+    isr_adc_Disable();  
     
     CyGlobalIntEnable; 
-    LED_Write(0);  // for debug
-    LED4_Write(0);  // for debug
-    
-    LCD_Start();  // for debug
-    LCD_Position(0,0);  // for debug
-    LCD_PrintString("Amp rebuild");  // for debug
     
     USBFS_Start(0, USBFS_5V_OPERATION);  // initialize the USB
     
     while(!USBFS_bGetConfiguration());  //Wait till it the usb gets its configuration from the PC ??
     USBFS_EnableOutEP(OUT_ENDPOINT);  // changed
     
-    for (int i=0; i<MAX_LUT_SIZE; i++)
-    {
-        ADC_array[1][i] = i;
-    }
+    LCD_Position(0,0);
+    LCD_PrintString("amp build3");
     
     for(;;)
     {
         /* check if there is a response from the computer */
         CheckInput();
 
+        if(USBFS_IsConfigurationChanged())
+        {
+            while(!USBFS_GetConfiguration());
+
+            USBFS_EnableOutEP(OUT_ENDPOINT);
+        }     
+
         if (Input_Flag == true)
         {
             switch (OUT_Data_Buffer[0]) 
-            {  // OUT is in reference from out of the computer,
-            // to the PSoC, the OUT data is actually input
+            {  
             case 'E': ; // User wants to export the data, the user can choose what ADC array to export
-                //LED_Write(1);  // for debug
                 uint8 user_ch = OUT_Data_Buffer[1]-'0';
+                
                 if (user_ch <= ADC_channels)  // check for buffer overflow
                 {
-                    LED_Write(0);
                     Export_Data(ADC_array[user_ch], waveform.length_total);
-                    LED_Write(1);
-                    LCD_Position(0,0);
-                    LCD_PrintString("Done Export");
                 }
                 else
                 {
-                    LCD_Position(0,0);
-                    LCD_PrintString("Buff Ovrflw req");
+                    USB_load_packet((uint8*)"Error Exporting", 32);
                 }
                 break;
             case 'C': ;  // change the compare value of the PWM to start the adc isr
-                uint16 CMP = Convert2Dec(&OUT_Data_Buffer[12], 5);
+                uint16 CMP = Convert2Dec(&OUT_Data_Buffer[2], 5);
                 PWM_isr_WriteCompare(CMP);
                 break;
-            case 'A': ; // input a 0-7 string into the device
+            case 'A': ; // input a 0-7 string into the device that will change the TIA resistor
                 uint8 TIA_resistor_value = OUT_Data_Buffer[1]-'0';
                 TIA_SetResFB(TIA_resistor_value);  // see TIA.h for how the values work, basically 0 - 20k, 1 -30k, etc.
+                ADC_SigDel_SetBufferGain(OUT_Data_Buffer[3]-'0');
                 break;
             case 'R': ;
-                LCD_Position(1,0);
-                LCD_PrintString("Running");
                 if (!isr_dac_GetState())  // enable the dac isr if it isnt already enabled
                 {
-                    isr_dac_Enable();
-                    isr_adc_Enable();
+                    /* set the electrode voltage to the lowest voltage first, then start ADC and wait for it to initiate
+                    before starting the interrupts  */
+                    IDAC_msb_SetValue(msb_lut_current_value);
+                    IDAC_lsb_SetValue(lsb_lut_current_value);
+                    CyDelay(1);  // let the electrode voltage settle
+                    ADC_SigDel_Start();
+                    ADC_SigDel_StartConvert();  // start the converstion process of the delta sigma adc so it will be ready to read when needed
+                    //  Make sure the delta sigma is ready before you call an interrupt
+                    // this breaks the device, will stop the mcu when a second run is given
+                    // ADC_SigDel_IsEndConversion(ADC_SigDel_WAIT_FOR_RESULT);  // probably not needed but just to be sure
+                    CyDelay(1);
+                    PWM_isr_WriteCounter(0x01u);  // set the pwm timer so that it will trigger adc isr first
+                    
+                    isr_dac_Enable();  // enable the interrupts to start the dac
+                    isr_adc_Enable();  // and the adc
                 }
                 break;
             case 'X': ; // reset the device by disabbleing isrs
                 isr_dac_Disable();
                 isr_adc_Disable();
-                lut_index = 0;     
+                lut_index = 0;  
+                break;
+            case 'I': ;  // identify the device and test if the usb is working properly
+                //send_message("USB Test", 32);
+                USB_load_packet((uint8*)"USB Test", 32);
+                break;
             case 'S': ; // getting the starting bit of the dacs, use ; after : to make an empty statement to prevent error
                 /*Get the string of data that contains the start and end voltges of the
                 triangle wave and produce the memory */
@@ -190,10 +188,6 @@ int main()
                 waveform.divider = Convert2Dec(&OUT_Data_Buffer[12], 5);
                 PWM_isr_WritePeriod(waveform.divider);
                 
-                // put the data in a string to be put on the LCD screen
-                sprintf(LCD_str, "s:%i |", waveform.divider);
-                LCD_Position(1,0);
-                LCD_PrintString(LCD_str);
                 MakeTriangleWave();
                 break; 
             }  // end of switch statment
@@ -203,20 +197,19 @@ int main()
     }  // end of for loop in main
 }  // end of main
 
-
-void send_message(char* message, uint8 n)
+void hardware_setup(void)
 {
-    strncpy(IN_Data_Buffer, message, n);
-    while(USBFS_GetEPState(IN_ENDPOINT) != USBFS_IN_BUFFER_EMPTY)
-    {
-        
-    }
-    LED4_Write(1);  // for debug  
-    if(USBFS_GetEPState(IN_ENDPOINT) == USBFS_IN_BUFFER_EMPTY)
-    {
-        USBFS_LoadEP(IN_ENDPOINT, IN_Data_Buffer, MAX_NUM_BYTES);
-        USBFS_EnableOutEP(OUT_ENDPOINT);
-    }
+    ADC_SigDel_Start();
+    TIA_Start();
+    VDAC_TIA_Start();
+    Timer_Start();
+    IDAC_msb_Start();
+    IDAC_lsb_Start();
+    PWM_isr_Start();
+    Opamp_Aux_Start();
+    AMux_Init();
+    AMux_FastSelect(0);
+    LCD_Start();
 }
 
 void CheckInput(void)
@@ -230,7 +223,6 @@ void CheckInput(void)
         /* Re-enable OUT endpoint */
         USBFS_EnableOutEP(OUT_ENDPOINT);
         /* Signal that the LCD display should be updated */
-        OUT_EP_count++;  // for debug
         Input_Flag = true;
     }
 }
@@ -274,97 +266,39 @@ void MakeTriangleWave(void)
     lsb_lut_current_value = lsb_waveform_lut[lut_index];
 }
 
-void ArrayCheck(void)  // for debug
-{
-    //LED3_Write(1);
-    CyDelay(1000);
-    //LED3_Write(0);
-}
-
 void Export_Data(int16 array[], int size)
 {
-    LCD_Position(0,0);
-    sprintf(LCD_str, "Exp Sz:%i |  ", size);
-    LCD_PrintString(LCD_str);
     int pre_buffer_index = 0;
-    uint8 pre_buffer[MAX_NUM_BYTES];
+    union char_int16_union pre_buffer;
+    //uint8 pre_buffer[MAX_NUM_BYTES];
     for(int i = 0; i <= size; i++)  // have to go through all the data and send it out the usb
     { // check if the old buffer was filled and sent and now needs to be cleared
-        
-        if (i % MAX_DATA_BUFFER == 0)   
+        if (i % (MAX_NUM_BYTES / 2) == 0)   
         {
             pre_buffer_index = 0;  //clear the buffer_index
-            for (int j=0; j < MAX_NUM_BYTES; j++)
-            {
-                pre_buffer[j] = 0;  // clear the pre_buffer
-            }
         }
-        // convert the uint16 data to two uint8 bytes to send
-        pre_buffer[2*pre_buffer_index] = (array[i] >> 8);
-        pre_buffer[2*pre_buffer_index + 1] = array[i] & 0xFF;
+        // put the int16 data into the int16 part of the char_int16_union array
+        pre_buffer.usb_int_data[pre_buffer_index] = array[i];
         pre_buffer_index++;
-//        LCD_Position(0,0);
-//        sprintf(LCD_str, "pre_buf_ind: %i  |", pre_buffer_index);
-//        LCD_PrintString(LCD_str);
-        //CyDelay(500);
         
         if ((i % MAX_DATA_BUFFER) == (MAX_DATA_BUFFER-1))  // the pre buffer is full
         {  // load the pre buffer into the usb hub to send it
-            LED4_Write(0);  // for debug  
-            while(USBFS_GetEPState(IN_ENDPOINT) != USBFS_IN_BUFFER_EMPTY)
-            LED4_Write(1);  // for debug  
-            if(USBFS_GetEPState(IN_ENDPOINT) == USBFS_IN_BUFFER_EMPTY)
-            {
-                USBFS_LoadEP(IN_ENDPOINT, pre_buffer, MAX_NUM_BYTES);
-                USBFS_EnableOutEP(OUT_ENDPOINT);
-            }
-            LCD_Position(1,0);
-            sprintf(LCD_str, "ExpC:%i|%i|  ", pre_buffer_index, i);
-            LCD_PrintString(LCD_str);
+            USB_load_packet(pre_buffer.usb_char_data, MAX_NUM_BYTES);
         }
-        
-        //button_wait();
     }
     // send last chunk of data out
-    LCD_Position(1,0);
-    sprintf(LCD_str, "ExpC:%i |  ", size);
-    //LED4_Write(0);  // for debug
+    USB_load_packet(pre_buffer.usb_char_data, MAX_NUM_BYTES);
+}
+
+void USB_load_packet(uint8 packetData[], uint16 length)
+{
     while(USBFS_GetEPState(IN_ENDPOINT) != USBFS_IN_BUFFER_EMPTY)
     {
-        
     }
-    //LED4_Write(1);  // for debug
     if(USBFS_GetEPState(IN_ENDPOINT) == USBFS_IN_BUFFER_EMPTY)
     {
-        USBFS_LoadEP(IN_ENDPOINT, pre_buffer, MAX_NUM_BYTES);
+        USBFS_LoadEP(IN_ENDPOINT, packetData, length);
         USBFS_EnableOutEP(OUT_ENDPOINT);
-    }
-    //LCD_Position(1,0);
-    //sprintf(LCD_str, "ExpC:%i |  ", size);
-    //LCD_PrintString(LCD_str);
-    debug_LCD(1, size, 0, 1);
-}
-
-void debug_LCD(uint8 row, uint var1, uint var2, uint test_num)
-{
-    LCD_Position(row,0);
-    sprintf(LCD_str, "t%i:%i|%i ", test_num, var1, var2);
-    LCD_PrintString(LCD_str);
-}
-
-void button_wait(void)
-{
-    //LCD_Position(1,0);
-    //LCD_PrintString("Waiting for SW3");
-    for(;;)
-    {
-        if(SW3_Read()==0)
-        {
-            //LCD_Position(1,0);
-            //LCD_PrintString("Read SW3");
-            CyDelay(200);
-            return;
-        }
     }
 }
 
